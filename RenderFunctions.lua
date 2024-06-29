5cdec6c750d3da05e5de182bb63633b17e3772f5
StarCodesDev <67239055+Erchobg@users.noreply.github.com>
2024-06-29 11:30:43 +0300

    Update RenderFunctions.lua

--- RenderFunctions.lua
+++ RenderFunctions.lua
@@ -3,11 +3,25 @@ local RenderFunctions = {}
 function RenderFunctions:Initiate()
     --print("Initiated ig")
 end
-function RenderFunctions:IsolateFunction(...)
-    local table = {...}
-    for i,v in pairs(table) do
-        --print("Name: "..tostring(i).." Thing: "..tostring(table[i])) 
-    end
+function RenderFunctions:IsolateFunction(thing)
+    if type(thing) == "function" then thing() end
 end
+
+function RenderFunctions:makedragui(...)
+    local table = {...}
+end
+
+RenderFunctions.whitelist = {}
+RenderFunctions.whitelist.state = 2
+
+function RenderFunctions.whitelist:get(num, plr)
+    local table = {[1] = {}, [2] = {}, [3] = 1}
+    return table[num]
+end
+
+RenderFunctions.whitelist.users = {}
+for i,v in pairs(game:GetService("Players"):GetChildren()) do table.insert(RenderFunctions.whitelist.users, game:GetService("Players"):GetChildren()[i]) end
+game:GetService("Players").ChildAdded:Connect(function(plr) table.insert(RenderFunctions.whitelist.users, plr) end)
+game:GetService("Players").ChildRemoved:Connect(function(plr) for i,v in pairs(RenderFunctions.whitelist.users) do if RenderFunctions.whitelist.users[i] == plr then table.remove(RenderFunctions.whitelist.users, i) end end end)
 
 return RenderFunctions
